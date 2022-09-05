d90dc6f64eb22dbc06bc934f4a8f94f19f7cffd9
peakiz <1957939650@qq.com>
2022-09-05 17:35:28 +0800

    init

---
+++ src/Help.java
@@ -0,0 +1,124 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Help {
+    //快排模板，填坑法
+    static private void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {   //递归出口，左等于右说明划分到只剩一个元素了
+            return;
+        }
+        int partitionIndex = partition(arr, left, right);
+        quickSort(arr, left, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, right);
+    }
+
+
+    static  private  int partition(int a[], int left, int right){
+        int pivot = a[left];//最左侧为枢轴，其实算是记录了left这个位置的数值，这个位置此时可以随意被其他数填充
+        while(left < right){
+            while(a[right] >= pivot && left  < right) right--;
+            a[left] = a[right];
+            while(a[left] < pivot && left < right) left++;
+            a[right] = a[left];
+        }
+        a[left] = pivot;//将记录的数值填充回去
+        return left;
+    }
+
+
+
+
+    //打印数组
+    static  void printArray(int[] nums){
+        StringBuilder sb = new StringBuilder("[");
+        for(int i = 0; i < nums.length; i++){
+            sb.append(nums[i]+",");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+    static  void printArray_2D(int[][] nums){
+        for(int i = 0; i < nums.length; i++){
+            printArray(nums[i]);
+        }
+
+    }
+
+
+
+
+    //构造链表：输入字符串，输出链表头
+    static ListNode creatListNode(String s){
+        String [] listNode = s.substring(1,s.length()-1).split(",");
+        ListNode head0 = new ListNode(Integer.parseInt(listNode[0]));
+        ListNode head = head0;
+        for (String e: listNode) {
+            head0.next = new ListNode(Integer.parseInt(e));
+            head0 = head0.next;
+        }
+        return head.next;
+    }
+
+    //打印链表
+    static  void printListNode(ListNode head0){
+        ListNode head = head0;
+        System.out.print("[");
+        while(head != null){
+            if(head.next == null){
+                System.out.print(head.val+"]");
+
+            }
+            else{
+                System.out.print(head.val+",");
+
+            }
+            head = head.next;
+
+        }
+
+    }
+    //创建数，输入序列化字符串，返回root节点，水平遍历，叶子节点的左右直接点为null  如[1,2,null,null,3,null,null]
+    static TreeNode creatTree(String data) {
+        if(data.equals("[]")) return null;
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>() {{ add(root); }};
+        int i = 1;
+        while(!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if(!vals[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if(!vals[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    //打印树：层次遍历,输入root，层次打印树
+    static void printTree(TreeNode root) {
+        if(root == null) System.out.println("[]");
+        StringBuilder sb = new StringBuilder("[");
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp != null){
+                sb.append(root.val+",");
+                q.offer(temp.left);
+                q.offer(temp.right);
+            }else{
+                sb.append("null,");
+            }
+
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,34 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    //思路：map去重统计词频，链表数组记录
+//时间复杂度：O(n + k)O(n+k)，其中 nn 是字符串 ss 的长度，kk 是字符串 ss 包含的不同字符的个数。
+// 遍历字符串统计每个字符出现的频率需要 O(n)O(n) 的时间。
+// 创建桶并将不同字符加入桶需要 O(k)O(k) 的时间。
+// 生成排序后的字符串，需要 O(k)O(k) 的时间遍历桶，以及 O(n)O(n) 的时拼接字符串间。
+// 因此总时间复杂度是 O(n + k)O(n+k)。
+
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        char[] bucket = new char[s.length()+1]; //list 数组 数组里存的是list list里存的是char 频率最多为n次
+        for(char key : map.keySet()){ //最多有k个不同的字符 bucket下标对应频率
+            int f = map.get(key);
+            bucket[f] = key;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = bucket.length - 1; i >= 0; i--){ //从频率最高的开始遍历
+            if(bucket[i] != 0){ //char数组默认为'0'
+                for(int j = 0; j < i; j++)  //字符c出现i次
+                    sb.append(bucket[i]); //bucket[i]就是出现i次的字符
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
